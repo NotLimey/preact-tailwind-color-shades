87e6f42a2a1f53f1e8bb6a5122c5c3bb9fca20b3
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-23 11:30:03 +0200

    max toasts

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,33 @@
 import { h } from 'preact';
 import { Route, Router } from 'preact-router';
-import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import toast, { Toaster, useToasterStore } from 'react-hot-toast';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 
-const App = () => (
-	<div id='app'>
-		<Toaster position='top-center' reverseOrder={false} />
-		<Router>
-			<Route path='/' component={Home} />
-		</Router>
-	</div>
-);
+const App = () => {
+	const { toasts } = useToasterStore();
+
+	useEffect(() => {
+		toasts
+			.filter((t) => t.visible) // Only consider visible toasts
+			.filter((_, i) => i >= 3) // Is toast index over limit?
+			.forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
+	}, [toasts]);
+
+	return (
+		<div id='app'>
+			<Toaster
+				position='top-center'
+				reverseOrder={false}
+				toastOptions={{}}
+			/>
+			<Router>
+				<Route path='/' component={Home} />
+			</Router>
+		</div>
+	);
+};
 
 export default App;
